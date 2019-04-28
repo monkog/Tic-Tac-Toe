c65f04b1782d0a9497f839b271549ddeb91accd5
monkog <monkog@wp.pl>
2019-04-28 15:38:27 +0200

    Add MouseMessageHandler unit tests

--- TicTacToeTests/MouseEventsHandling/MouseMessageHandlerTests.cs
+++ TicTacToeTests/MouseEventsHandling/MouseMessageHandlerTests.cs
@@ -1,12 +1,41 @@
-﻿using NUnit.Framework;
+﻿using System.Windows.Forms;
+using NUnit.Framework;
+using TicTacToe.MouseEventsHandling;
 
 namespace TicTacToeTests.MouseEventsHandling
 {
 	public class MouseMessageHandlerTests
 	{
-		[Test]
-		public void TestMethod1()
+		private MouseMessageHandler _unitUnderTest;
+
+		[SetUp]
+		public void Initialize()
 		{
+			_unitUnderTest = new MouseMessageHandler();
+		}
+
+		[Test]
+		[TestCase(MouseEvent.LeftButtonDown)]
+		[TestCase(MouseEvent.LeftButtonUp)]
+		[TestCase(MouseEvent.RightButtonDown)]
+		[TestCase(MouseEvent.RightButtonUp)]
+		public void PreFilterMessage_KnownMouseEvent_True(int m)
+		{
+			var message = new Message { Msg = m };
+
+			var result = _unitUnderTest.PreFilterMessage(ref message);
+
+			Assert.IsTrue(result);
+		}
+
+		[Test]
+		public void PreFilterMessage_UnknownMouseEvent_False()
+		{
+			var message = new Message { Msg = 0 };
+
+			var result = _unitUnderTest.PreFilterMessage(ref message);
+
+			Assert.IsFalse(result);
 		}
 	}
 }
